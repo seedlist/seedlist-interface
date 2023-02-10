7fbbd92f3f5b5451ddda00f9039c2816c632a27e
ebbe52e5 <ebbe52e5@protonmail.com>
2023-02-10 17:27:17 +0800

    change local text

--- src/pages/nostrid/index.tsx
+++ src/pages/nostrid/index.tsx
@@ -21,7 +21,7 @@ const NostrId:React.FC<IBaseProps> = (props:IBaseProps)=>{
 
 
 				<Center>
-						<Box bgColor="#2b2d32" p="5" w="90%" maxW="lg" borderRadius="8" >
+						<Box bgColor="#2b2d32" p="5" w="95%" maxW="lg" borderRadius="8" >
 							<Stack spacing={6} w="100%">
 								<Center>
 									<Text fontSize="2xl" fontWeight="extrabold"><Trans>Race List</Trans>:</Text>
@@ -29,37 +29,37 @@ const NostrId:React.FC<IBaseProps> = (props:IBaseProps)=>{
 								<Flex >
 									<Center flex="1" h="22px"  fontWeight="extrabold" marginY="0px"> apple.j </Center>
 									<Spacer />
-									<Center flex="1" h="22px" fontWeight="extrabold"> <Trans>Finished </Trans></Center>
+									<Center flex="1" h="22px" fontWeight="extrabold"> <Trans>Finished</Trans></Center>
 									<Spacer />
-									<Center flex="1" h="22px" fontWeight="extrabold"><Trans>Deal</Trans> ￥126 </Center>
+									<Center flex="1" h="22px" fontWeight="extrabold"><Trans>Deal</Trans> ￥126</Center>
 								</Flex>
 								<Flex >
 									<Center flex="1" h="22px" fontWeight="extrabold"> google.j </Center>
 									<Spacer />
 									<Center flex="1" h="22px" fontWeight="extrabold"> <Trans>Finished </Trans></Center>
 									<Spacer />
-									<Center flex="1" h="22px" fontWeight="extrabold"> <Trans>Deal</Trans>￥217 </Center>
+									<Center flex="1" h="22px" fontWeight="extrabold"> <Trans>Deal</Trans> ￥217</Center>
 								</Flex>
 								<Flex backgroundColor="#975A16">
 									<Center flex="1" h="22px" fontWeight="extrabold"> amazon.j </Center>
 									<Spacer />
 									<Center flex="1" h="22px" fontWeight="extrabold"> <Trans>Bidding </Trans></Center>
 									<Spacer />
-									<Center flex="1" h="22px" fontWeight="extrabold"><Trans>Now</Trans>￥311</Center>
+									<Center flex="1" h="22px" fontWeight="extrabold"><Trans>Now</Trans> ￥311</Center>
 								</Flex>
 								<Flex>
 									<Center flex="1" h="22px" fontWeight="extrabold"> openai.j </Center>
 									<Spacer />
 									<Center flex="1" h="22px" fontWeight="extrabold"> <Trans>Coming </Trans></Center>
 									<Spacer />
-									<Center flex="1" h="22px" fontWeight="extrabold"><Trans>Init</Trans> ￥10 </Center>
+									<Center flex="1" h="22px" fontWeight="extrabold"><Trans>Init</Trans> ￥10</Center>
 								</Flex>
 								<Flex>
 									<Center flex="1" h="22px" fontWeight="extrabold"> joule.j </Center>
 									<Spacer />
-									<Center flex="1" h="22px" fontWeight="extrabold"><Trans> Coming </Trans></Center>
+									<Center flex="1" h="22px" fontWeight="extrabold"><Trans>Coming</Trans></Center>
 									<Spacer />
-									<Center flex="1" h="22px" fontWeight="extrabold"><Trans>Init</Trans> ￥50 </Center>
+									<Center flex="1" h="22px" fontWeight="extrabold"><Trans>Init</Trans> ￥50</Center>
 								</Flex>
 							</Stack>
 						</Box>
